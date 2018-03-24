8bef2b493d1f02ae0a6d04b985aaa45d296b616c
Eneylton <eneylton@hotmail.com>
2018-03-24 18:44:37 -0300

    Criando Instaciação

--- src/main/java/com/CrudSpringRest/CrudSpringRestApplication.java
+++ src/main/java/com/CrudSpringRest/CrudSpringRestApplication.java
@@ -1,12 +1,35 @@
 package com.CrudSpringRest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.CrudSpringRest.model.Categoria;
+import com.CrudSpringRest.repository.CategoriaRepository;
+import com.mysql.fabric.xmlrpc.base.Array;
+
 @SpringBootApplication
-public class CrudSpringRestApplication {
+public class CrudSpringRestApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringRestApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 }
